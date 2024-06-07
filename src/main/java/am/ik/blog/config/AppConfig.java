0008ba5ab1840da2fcd196711d806b4e0185f6be
Toshiaki Maki <makingx@gmail.com>
2024-06-07 11:02:14 +0900

    Drop noisy spans

--- src/main/java/am/ik/blog/config/AppConfig.java
+++ src/main/java/am/ik/blog/config/AppConfig.java
@@ -7,7 +7,13 @@ import am.ik.accesslogger.AccessLogger;
 import am.ik.accesslogger.AccessLoggerBuilder;
 import am.ik.spring.http.client.RetryableClientHttpRequestInterceptor;
 import io.micrometer.core.instrument.config.MeterFilter;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.actuate.web.exchanges.HttpExchange;
 import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
@@ -57,6 +63,25 @@ public class AppConfig {
 			return uri != null && (uri.startsWith("/readyz") || uri.startsWith("/livez") || uri.startsWith("/actuator")
 					|| uri.startsWith("/cloudfoundryapplication"));
 		});
+	}
+
+	@Bean
+	public static BeanPostProcessor ruleBasedRoutingSampler() {
+		return new BeanPostProcessor() {
+			@Override
+			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+				if (bean instanceof Sampler) {
+					AttributeKey<String> uri = AttributeKey.stringKey("uri");
+					return RuleBasedRoutingSampler.builder(SpanKind.SERVER, (Sampler) bean)
+						.drop(uri, "^/readyz")
+						.drop(uri, "^/livez")
+						.drop(uri, "^/actuator")
+						.drop(uri, "^/cloudfoundryapplication")
+						.build();
+				}
+				return bean;
+			}
+		};
 	}
 
 }
